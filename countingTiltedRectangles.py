26d42dc15c22a55b59c88b662434cadc0ced2ea4
geraud <geraud.benazet@gmail.com>
2019-12-02 22:12:16 +0100

    Added code to find tilted rectangles

---
+++ countingTiltedRectangles.py
@@ -0,0 +1,74 @@
+# https://www.youtube.com/watch?v=EuPSibuIKIg
+# Google Coding Interview With A Competitive Programmer
+
+import time
+
+points = [
+    (0, 0), (0, 1),
+    (1, 0), (1, 1),
+    (2, 0), (2, 1), (2, 2),
+    (0, 2),
+    (3, 1), (3, 3), (1, 3),
+    (4,2), (2,5)
+
+]
+
+# Display grid
+print('Count the rectangles defined by these points :')
+xmin = min(points)[0]
+xmax = max(points)[0]+1
+ymin = min(points,key= lambda t: t[1])[1]-1
+ymax = max(points,key= lambda t: t[1])[1]
+for y in range(ymax,ymin,-1):
+    line = '{:>2} '.format(y)
+    for x in range(xmin,xmax):
+        if (x, y) in points:
+            line += ". "
+        else:
+            line += "  "
+    print(line)
+line = '   '
+for x in range(xmin,xmax):
+    line += '{:<2}'.format(x)
+print(line)
+
+
+# My solution
+def orth(O, A, B):
+    # Dot Product = 0
+    return (A[0]-O[0])*(B[0]-O[0]) + (A[1]-O[1])*(B[1]-O[1]) == 0
+
+def CW(O,A,B):
+    # Cross product > 0
+    return (A[0]-O[0])*(B[1]-O[1]) - (A[1]-O[1])*(B[0]-O[0]) > 0
+
+t = time.time() * 1e6
+rects = []
+for p1 in points:
+    for p2 in points:
+        if p2[0] > p1[0] and p2[1] >= p1[1]:
+            for p4 in points:
+                if p4 != p1 and p4 != p2 and orth(p1, p2, p4) and CW(p1,p2,p4):
+                    for p3 in points:
+                        if p3 != p1 and p3 != p2 and p3 != p4 and orth(p3, p2, p4) and orth(p2, p1, p3):
+                            rects.append((p1, p2, p3, p4))
+
+t = time.time()*1e6-t
+
+# Display results
+
+for rect in rects:
+    print()
+    for y in range(ymax,ymin,-1):
+        line = ''
+        for x in range(xmin,xmax):
+            if (x, y) in rect:
+                line += repr(rect.index((x,y))+1) + " "
+            else:
+                if (x, y) in points:
+                    line += ". "
+                else:
+                    line += "  "
+        print(line)
+
+print("\n%d rectangles found in %d µs" % (len(rects), t))
