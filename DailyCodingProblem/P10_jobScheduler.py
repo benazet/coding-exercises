969a91739833270bd7749fb5ee259cd407caf867
geraud <geraud.benazet@gmail.com>
2019-12-27 19:04:50 +0100

    P10, bad solution with no internet

---
+++ DailyCodingProblem/P10_jobScheduler.py
@@ -0,0 +1,19 @@
+# Good morning! Here's your coding interview problem for today.
+# This problem was asked by Apple.
+# Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
+
+import time
+
+def f():
+    print("Function f called")
+
+
+def schedule(f,n):
+    t = time.time()
+    while time.time() - t < n:
+        #waiting
+        print("Waiting")
+    f()
+
+schedule(f)
